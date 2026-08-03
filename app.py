9fbd328321a67db3ffa071ca66f68d897ff4c7bc
obadonistanley <obadoni.stan@gmail.com>
2026-08-03 21:11:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import uvicorn
 
 app = FastAPI(title="Supastan AI Liquidity Scanner")
 
@@ -9,3 +11,11 @@ def home():
         "scanner": "Supastan AI Liquidity Scanner",
         "version": "1.0"
     }
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port
+    )
