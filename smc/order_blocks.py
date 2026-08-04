1d62218c89e2f308ff08bb6edf109a9a4785204c
obadonistanley <obadoni.stan@gmail.com>
2026-08-04 01:46:29 +0100

    Update order_blocks.py

--- smc/order_blocks.py
+++ smc/order_blocks.py
@@ -3,20 +3,62 @@ class OrderBlock:
     def __init__(self):
         pass
 
+
     def detect(self, candles):
 
-        if len(candles) < 4:
+        if len(candles) < 10:
             return None
 
-        last = candles[-1]
-        prev = candles[-2]
+
+        ob_candle = candles[-2]
+        confirmation = candles[-1]
+
+
+        body = abs(
+            ob_candle["close"] - ob_candle["open"]
+        )
+
+
+        candle_range = (
+            ob_candle["high"] - ob_candle["low"]
+        )
+
+
+        if candle_range == 0:
+            return None
+
+
+
+        strength = body / candle_range
+
+
+
+        # Ignore weak candles
+        if strength < 0.5:
+            return None
+
+
 
         # Bullish Order Block
-        if prev["close"] < prev["open"] and last["close"] > prev["high"]:
+        # Last bearish candle before bullish displacement
+
+        if (
+            ob_candle["close"] < ob_candle["open"]
+            and confirmation["close"] > ob_candle["high"]
+        ):
             return "BUY"
 
+
+
         # Bearish Order Block
-        if prev["close"] > prev["open"] and last["close"] < prev["low"]:
+        # Last bullish candle before bearish displacement
+
+        if (
+            ob_candle["close"] > ob_candle["open"]
+            and confirmation["close"] < ob_candle["low"]
+        ):
             return "SELL"
+
+
 
         return None
