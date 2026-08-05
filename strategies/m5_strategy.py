1d31dfc37f14e1de80bc334326c1fffea532f6c5
obadonistanley <obadoni.stan@gmail.com>
2026-08-05 01:39:54 +0100

    Update m5_strategy.py

--- strategies/m5_strategy.py
+++ strategies/m5_strategy.py
@@ -1,1 +1,60 @@
+from scanner import Scanner
+from data.deriv import DerivAPI
 
+
+class M5Strategy:
+
+    def __init__(self):
+
+        self.scanner = Scanner()
+        self.deriv = DerivAPI()
+
+    def run(self, symbol):
+
+        m5 = self.deriv.get_candles(
+            symbol,
+            count=250,
+            granularity=300
+        )
+
+        m1 = self.deriv.get_candles(
+            symbol,
+            count=250,
+            granularity=60
+        )
+
+        if not m5 or not m1:
+
+            return {
+                "final_signal": "NO DATA"
+            }
+
+        m5_analysis = self.scanner.scan(m5)
+
+        m1_analysis = self.scanner.scan(m1)
+
+        final_signal = "NO TRADE"
+
+        if (
+            m5_analysis["signal"] == "BUY"
+            and m1_analysis["signal"] == "BUY"
+        ):
+            final_signal = "BUY"
+
+        elif (
+            m5_analysis["signal"] == "SELL"
+            and m1_analysis["signal"] == "SELL"
+        ):
+            final_signal = "SELL"
+
+        return {
+
+            "strategy": "M5",
+
+            "final_signal": final_signal,
+
+            "trend": m5_analysis,
+
+            "confirmation": m1_analysis
+
+        }
