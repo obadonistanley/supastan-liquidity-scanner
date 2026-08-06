c4b52da2544ca92d26f20116a7613e86c9217bbe
obadonistanley <obadoni.stan@gmail.com>
2026-08-06 11:02:54 +0100

    Update signals_store.py

--- signals_store.py
+++ signals_store.py
@@ -2,15 +2,23 @@ signals = []
 
 
 def add_signal(signal):
-
     global signals
+
+    # Remove duplicate
+    signals = [
+        s for s in signals
+        if not (
+            s["symbol"] == signal["symbol"]
+            and s["timeframe"] == signal["timeframe"]
+            and s["signal"] == signal["signal"]
+        )
+    ]
 
     signals.insert(0, signal)
 
-    # Keep only the latest 100 signals
+    # Keep latest 100 signals
     signals = signals[:100]
 
 
 def get_signals():
-
     return signals
