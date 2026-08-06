d8e4e77ec99be362903ded496198ffe8f9679ca7
obadonistanley <obadoni.stan@gmail.com>
2026-08-06 14:53:27 +0100

    Update background.py

--- background.py
+++ background.py
@@ -1,12 +1,2 @@
 def get_timeframes(symbol):
-
-    if symbol in FOREX:
-        return ["D1", "H4", "H1"]
-
-    if symbol in DERIV:
-        return ["H4", "H1", "M5"]
-
-    if symbol in INDICES:
-        return ["H4", "H1"]
-
-    return ["H1"]
+    return ["M5"]
