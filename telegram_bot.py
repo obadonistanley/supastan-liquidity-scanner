e49e1bdba390be19b4fb899df14cc218128980f7
obadonistanley <obadoni.stan@gmail.com>
2026-08-06 13:46:30 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,6 +2,8 @@ import os
 import requests
 from datetime import datetime, UTC
 
+from chart import create_chart
+
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
@@ -13,7 +15,9 @@ def send_signal(result):
         print("❌ Telegram credentials missing")
         return False
 
+
     liquidity = result.get("liquidity", {})
+
 
     message = f"""
 🚨 SUPASTAN AI LIQUIDITY SWEEP ALERT
@@ -41,26 +45,77 @@ def send_signal(result):
 ⚡ Supastan AI Liquidity Scanner
 """
 
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     try:
 
-        response = requests.post(
-            url,
-            data={
-                "chat_id": CHAT_ID,
-                "text": message
-            },
-            timeout=10
-        )
+        # create chart
+        candles = result.get("candles")
+
+        chart_file = None
+
+        if candles:
+            chart_file = create_chart(
+                candles,
+                result
+            )
+
+
+        # send image if available
+        if chart_file:
+
+            url = (
+                f"https://api.telegram.org/"
+                f"bot{BOT_TOKEN}/sendPhoto"
+            )
+
+
+            with open(chart_file, "rb") as image:
+
+                response = requests.post(
+                    url,
+                    data={
+                        "chat_id": CHAT_ID,
+                        "caption": message
+                    },
+                    files={
+                        "photo": image
+                    },
+                    timeout=20
+                )
+
+
+        else:
+
+            url = (
+                f"https://api.telegram.org/"
+                f"bot{BOT_TOKEN}/sendMessage"
+            )
+
+
+            response = requests.post(
+                url,
+                data={
+                    "chat_id": CHAT_ID,
+                    "text": message
+                },
+                timeout=10
+            )
+
 
         if response.status_code == 200:
             print("✅ Telegram alert sent")
             return True
 
+
         print("Telegram Error:", response.text)
         return False
 
+
     except Exception as e:
-        print("Telegram Exception:", e)
+
+        print(
+            "Telegram Exception:",
+            e
+        )
+
         return False
