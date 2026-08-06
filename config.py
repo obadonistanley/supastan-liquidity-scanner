e7c8af2439b3bc41a5f3393da82e50e2b8da82b1
obadonistanley <obadoni.stan@gmail.com>
2026-08-06 02:08:44 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,32 @@
+MARKETS = [
 
+    "GBPUSD",
+    "GBPJPY",
+    "EURJPY",
+    "NZDJPY",
+    "EURUSD",
+    "USDCAD",
+    "GBPNZD",
+
+    "XAUUSD",
+    "BTCUSD",
+
+    "R_10",
+    "R_10 1s",
+
+    "R_25",
+    "R_25 1s",
+
+    "R_50",
+
+    "R_75",
+    "R_75 1s",
+
+    "R_100",
+
+    "R_150 1s",
+
+    "US30",
+    "NAS"
+
+]
